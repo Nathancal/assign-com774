d21145acac737a0b07d326489320a7584de55f56
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2023-12-19 12:30:11 +0000

    update dataset and algorithm

--- main.py
+++ main.py
@@ -3,29 +3,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load the WISDM dataset (replace 'path_to_data' with the actual path to your WISDM dataset)
-path_to_data = 'WISDM_ar_v1.1/'
-columns = ['user', 'activity', 'timestamp', 'x-axis', 'y-axis', 'z-axis']
-df = pd.read_csv(path_to_data + 'WISDM_ar_v1.1_raw.txt', header=None, names=columns, comment=';')
+df = pd.read_csv('hardata.csv')
 
 df = df.dropna()
 
-# Preprocess the data
-print(df['z-axis'])
-
-df['z-axis'] = df['z-axis'].astype(float)
-
 # Extract features and labels
-X = df[['x-axis', 'y-axis', 'z-axis']]
+X =  df.drop('activity', axis=1)
 y = df['activity']
 
-X_subset, _, y_subset, _ = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+print(y)
+
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_subset, y_subset, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a Random Forest Classifier
-clf = RandomForestClassifier(n_estimators=1000, random_state=42)
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
 
 # Make predictions on the test set
