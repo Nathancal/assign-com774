5faa36894433ae04016db0739287d83a2e40ee5c
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2023-12-30 21:52:23 +0000

    update servber

--- federated_learning/server/server.py
+++ federated_learning/server/server.py
@@ -7,15 +7,17 @@ import argparse
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 
+
+ml_client = MLClient.from_config(credential=DefaultAzureCredential())
+
 # Get the arugments we need to avoid fixing the dataset path in code
 parser = argparse.ArgumentParser()
 parser.add_argument("--trainingdata", type=str, required=True, help='Training data for model server')
 args = parser.parse_args()
 
-ml_client = MLClient.from_config(credential=DefaultAzureCredential())
+data_name = args.trainingdata
 
-
-data_asset = ml_client.data._get_latest_version(args.trainingdata)
+data_asset = ml_client.data._get_latest_version(data_name)
 
 # Load and preprocess combined HAR data
 X, Y = utils.load_har_data(data_asset)
