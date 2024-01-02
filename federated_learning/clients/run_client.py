786df2b0fa43c19343e086165c28cccf222aebb1
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2024-01-02 15:36:14 +0000

    update run client client id

--- federated_learning/clients/run_client.py
+++ federated_learning/clients/run_client.py
@@ -4,7 +4,7 @@ from azureml.core import Workspace, Run, Environment
 from azureml.core.model import InferenceConfig, Model
 from azureml.core.webservice import AciWebservice
 from azure.ai.ml import MLClient
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
 
 
 parser = argparse.ArgumentParser()
@@ -12,6 +12,15 @@ parser.add_argument("--total_subjects", type=int, required=True, help='Subject n
 args = parser.parse_args()
 
 subject_num = args.total_subjects
+
+# Replace with your actual values
+tenant_id = "6f0b9487-4fa8-42a8-aeb4-bf2e2c22d4e8"
+client_id = "1bee10b2-17dd-4a50-b8aa-488d27bdd5a1"
+client_secret = "MZK8Q~M5oNATdagyRKMUs-V-2dNggq3aAlRRdb8W"
+subscription_id = "092da66a-c312-4a87-8859-56031bb22656"
+
+credentials = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
+
 
 ml_client = MLClient.from_config(credential=DefaultAzureCredential())
 
