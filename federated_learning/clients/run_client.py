f967ab5f058ca7ba439326a8248b67f77992611e
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2024-01-03 18:06:37 +0000

    credentials

--- federated_learning/clients/run_client.py
+++ federated_learning/clients/run_client.py
@@ -1,7 +1,7 @@
 import argparse
 from azureml.core import Workspace, Environment, ScriptRunConfig, Experiment, Run, Dataset
 from azure.ai.ml import MLClient
-from azure.identity import DefaultAzureCredential
+from azure.identity import ClientSecretCredential
 from concurrent.futures import ProcessPoolExecutor
 import logging
 # Configure logger
@@ -18,13 +18,20 @@ subject_num = args.total_subjects
 tenant_id = "6f0b9487-4fa8-42a8-aeb4-bf2e2c22d4e8"
 client_id = "1bee10b2-17dd-4a50-b8aa-488d27bdd5a1"
 client_secret = "MZK8Q~M5oNATdagyRKMUs-V-2dNggq3aAlRRdb8W"
-subscription_id = "092da66a-c312-4a87-8859-56031bb22656"
-credential = DefaultAzureCredential(client_id=client_id, client_secret=client_secret)
 
-ws = Workspace.from_config(path='./config.json')
+
+subscription_id = "092da66a-c312-4a87-8859-56031bb22656"
+# Create a ServicePrincipalCredentials instance
+credentials = ClientSecretCredential(
+    client_id=client_id,
+    client_secret=client_secret,
+    tenant_id=tenant_id
+)
+
+ws = Workspace.from_config(path='./config.json', auth=credentials)
 environment = Environment.get(workspace=ws, name="development")
 
-ml_client = MLClient.from_config(credential=credential)
+ml_client = MLClient.from_config(credential=credentials)
 
 # Function to submit a job
 def submit_job(subject_num):
